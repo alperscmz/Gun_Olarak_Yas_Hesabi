9fc55fba1609ff2252a75dbc38ad6c42ee104255
alperscmz <alpersacmaozu@gmail.com>
2022-01-24 16:53:17 +0300

    Gun_Olarak_Yas_Hesabi
    vs c# ile Gun_Olarak_Yas_Hesabi

---
+++ YAS_HESAPLAMASİ/YAS_HESAPLAMASİ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAS_HESAPLAMASİ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan zaman;
+            int gunfarki;
+            DateTime tarih1, tarih2;
+            Console.WriteLine("Doğum tarhinizi giriniz: ");
+            tarih1 = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Bugünün tarihini giriniz: ");
+            tarih2 = Convert.ToDateTime(Console.ReadLine());
+            zaman = tarih2 - tarih1;
+            gunfarki = zaman.Days;
+            Console.WriteLine("Yaşınız: " + gunfarki);
+            Console.Read();
+
+        }
+    }
+}
